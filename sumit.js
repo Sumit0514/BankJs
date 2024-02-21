67b61e13e83166d7cc98d2670bdda75ca77d82d9
Sumit0514 <sumitkumarbarik2004@gmail.com>
2024-02-21 16:50:23 +0530

    1st

---
+++ sumit.js
@@ -0,0 +1,20 @@
+Total_Balance=500000;
+let mode={
+    ATM:150000,
+    UPI:100000,
+    Online:50000,
+    TapToPay:15000,
+}
+function withdrawl(amount,a){
+    if(amount<=a){
+        Total_Balance=Total_Balance-amount
+        console.log("TRANSACTION SUCCESSFUL")
+        console.log("TOTAL BALANCE LEFT IS "+Total_Balance)
+    }
+    else{
+        console.log("TRANSACTION FAILED")
+    }
+}
+withdrawl(10500,mode.TapToPay) //enter the amount fisrt and then the mode of transaction(mode.XYZ)
+
+
